59634ab8d5d50862fca97b87556ca8973f1a74ad
Sayyed Arslaan <arslaan.developer@gmail.com>
2026-05-24 20:20:17 +0530

    Update image source in Hero component
    jpeg to webpg

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -29,7 +29,7 @@ export default function Hero() {
           initial={{ opacity: 0, scale: 0.8 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.8 }}
-          src="https://res.cloudinary.com/dccm287b8/image/upload/v1779554275/Edit_this_photo_very_naturally_202605191401_g0slui.jpg"
+          src="https://res.cloudinary.com/dccm287b8/image/upload/v1779633817/copy_of_edit_this_photo_very_naturally_202605191401_g0slui.webp"
           alt="Profile"
           className="w-32 h-32 rounded-full border-2 border-neon-purple shadow-[0_0_20px_#bc13fe] object-cover mb-6"
         />
